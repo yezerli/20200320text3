b518541aadb788cbd5e246a384f68a06a2b712d1
yezerli <1123603008@qq.com>
2020-03-20 15:58:45 +0800

    前后交换完毕，接下来尝试去除连接线

---
+++ 20200320text3/20200320text3.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main() {
+	cv::Mat srcMat = imread("E:\\Git\\IMG_1989.jpg", 0);
+	//读取图片
+	if (srcMat.empty()) {
+		return -1;
+	}//读取失败
+	cv::Mat binaryMat;
+	threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+	cv::imshow("二值化", binaryMat);//显示二值化图像
+	//二值化
+	//准备开始对调背景前景
+	int height = binaryMat.rows;//表示行数
+	int width = binaryMat.cols;//表示每行元素的总元素数量
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			if (binaryMat.at<uchar>(j, i) == 0) {
+				binaryMat.at<uchar>(j, i) = 255;
+			}
+			else
+				binaryMat.at<uchar>(j, i) = 0;
+		}
+	}
+	imshow("前后交换后图", binaryMat);
+	//创建结构元素
+	/*Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	//闭运算
+	morphologyEx(binaryMat, binaryMat, MORPH_CLOSE, kernel);
+	cv::Mat labelMat;
+	cv::Mat statsMat;
+	cv::Mat centreMat;
+	cv::Mat resultMat;
+	int nccomps = cv::connectedComponentsWithStats(
+		binaryMat,
+		labelMat,
+		statsMat,
+		centreMat,
+		8,
+		CV_32S
+	);
+
+	for (int i = 1; i < nccomps; i++) {
+		cout << "connected Copmonents NO. " << i << endl;
+		cout << "pixels = " << statsMat.at<int>(i, 4) << endl;
+		cout << "width = " << statsMat.at<int>(i, 2) << endl;
+		cout << "height = " << statsMat.at<int>(i, 3) << endl;
+		cout << endl;
+	}
+	cout << "the number of connected Components = " << nccomps - 1 << endl;
+	//画bounding box
+	for (int i = 1; i < nccomps; i++) {
+		Rect bndbox;
+		bndbox.x = statsMat.at<int>(i, 0);
+		bndbox.y = statsMat.at<int>(i, 1);
+		bndbox.width = statsMat.at<int>(i, 2);
+		bndbox.height = statsMat.at<int>(i, 3);
+		//绘制
+		rectangle(binaryMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+	imshow("results", binaryMat);*/
+	waitKey(0);
+}
